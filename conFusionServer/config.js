86047d1966a7872023cb45861edc354ee09ef486
Deepak Khairwal <Deepak.Khairwal@shell.com>
2020-05-30 12:27:40 +0530

    d

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -3,10 +3,6 @@ module.exports = {
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
         clientId: '267184394694123',
-<<<<<<< HEAD
         clientSecret: '689c474c80e6bae63e17346c85c80392'
-=======
-        clientSecret: '689c474c80e6bae63e17346c85c80395'
->>>>>>> e7422f298c82075c6d6e909a42d7f14f352c3f27
     }
 }
